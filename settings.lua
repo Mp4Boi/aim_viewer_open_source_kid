57391624098b7f15091780dfd797be32c1f577fe
Mp4Boi <119469108+Mp4Boi@users.noreply.github.com>
2023-01-27 20:54:18 +0100

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,7 @@
+getgenv().enable = false -- this script is not made by me
+getgenv().color = Color3.fromRGB(255,0,0) -- red
+getgenv().toggle_keybind = "p" -- enable tracer the red thing 
+getgenv().swith_nigga = 'k' -- press k and u will see a noti on the user ur tracer is on 
+getgenv().method = "MousePos" 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mp4Boi/aim_viewer_open_source_kid/main/.lua")()
