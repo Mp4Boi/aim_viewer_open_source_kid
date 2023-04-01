36ad2183023a3220a96b008097f0282478428251
Mp4Boi <119469108+Mp4Boi@users.noreply.github.com>
2023-04-01 07:28:30 +0200

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -4,4 +4,4 @@ getgenv().toggle_keybind = "p" -- enable tracer the red thing
 getgenv().swith_nigga = 'k' -- press k and u will see a noti on the user ur tracer is on 
 getgenv().method = "MousePos" 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mp4Boi/aim_viewer_open_source_kid/main/.lua")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mp4Boi/aim_viewer_open_source_kid/main/.lua"))()
